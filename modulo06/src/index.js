01d5438947dfabf65c3e8c9485f5ff54f8d17c69
fpmacedo <lipe_macedo@msn.com>
2019-11-20 22:23:49 -0300

    Finalizada aula de loading disable

--- modulo06/src/index.js
+++ modulo06/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import './config/ReactotronConfig';
 import Routes from './routes';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <>
+      {/* altera a cor da status bar hora, data etc.. */}
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
+  );
 }
